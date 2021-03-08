bb8da8a23bef548b08c7d2c08f491ab7ffe19329
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-03-08 08:46:21 -0300

    Update Ex3_main.java

--- Ex3/Ex3_main.java
+++ Ex3/Ex3_main.java
@@ -1,22 +1,18 @@
 public class Ex3_main {
     public static void main(String[] args) {
 
-        Disciplina Matemática = new Disciplina();
-        Matemática.setNome("Matemática");
+        Disciplina Matemática = new Disciplina("Matemática");
         Matemática.setCarga_horaria(68);
 
-        Disciplina Geografia = new Disciplina();
-        Geografia.setNome("Geografia");
+        Disciplina Geografia = new Disciplina("Geografia");
         Geografia.setCarga_horaria(68);
 
-        Disciplina História = new Disciplina();
-        História.setNome("História");
+        Disciplina História = new Disciplina("História");
         História.setCarga_horaria(68);
         História.setHorarioAula("16:00h");
         System.out.println("Horário da aula de história: "+História.getHorarioAula());
 
-        Aluno Lucas = new Aluno();
-        Lucas.setNome("Lucas");
+        Aluno Lucas = new Aluno("Lucas");
         Lucas.AddHistorico(Matemática, 10);
         Lucas.AddHistorico(Geografia, 8);
         Lucas.AddHistorico(História, 9);
@@ -30,17 +26,15 @@ public class Ex3_main {
         Lucas.calculaCR(História, 9);
         System.out.println(Lucas.getCR());
 
-        Turma turma1 = new Turma();
+        Turma turma1 = new Turma(20);
         turma1.setMedia_da_turma(8);
         turma1.setMedia_aluno(Lucas, 6);
         System.out.print("Média de Lucas em relação ao resto da turma: ");
         System.out.println(turma1.getMediaDoAlunoNaTurma(Lucas));
 
-        Disciplina Arquiteturas_de_comp = new Disciplina();
-        Arquiteturas_de_comp.setNome("Arquiteturas de Computadores");
+        Disciplina Arquiteturas_de_comp = new Disciplina("Arquiteturas de Computadores");
         System.out.print("Ementa de Arquiteturas de Computadores: ");
         Arquiteturas_de_comp.setEmenta("*bastante coisa*");
         System.out.println(Arquiteturas_de_comp.getEmenta());
-
     }
 }
