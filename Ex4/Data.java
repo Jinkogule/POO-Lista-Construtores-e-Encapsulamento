101aec1430367808d3268c9f88edf4c089c13b13
Lucas Pimenta <52849575+Jinkogule@users.noreply.github.com>
2021-03-08 09:16:36 -0300

    Update Data.java

--- Ex4/Data.java
+++ Ex4/Data.java
@@ -5,22 +5,28 @@ public class Data {
     private String mes_escrito;
     int dias_no_ano;
 
-    public String data(int dia, int mes, int ano){
+    public Data(int dia, int mes, int ano){
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
-        return this.dia+"/"+this.mes+"/"+this.ano;
     }
-    public String data(String mes_escrito, int dia, int ano){
+    public Data(String mes_escrito, int dia, int ano){
         this.mes_escrito = mes_escrito;
         this.dia = dia;
         this.ano = ano;
-        return this.mes_escrito+" "+this.dia+", "+this.ano;
     }
-    public String data(int dias_no_ano, int ano){
+    public Data(int dias_no_ano, int ano){
         this.dias_no_ano = dias_no_ano;
         this.ano = ano;
-        return this.dias_no_ano+" "+this.ano;
     }
 
+    public String getData1(){
+        return this.dia+"/"+this.mes+"/"+this.ano;
+    }
+    public String getData2(){
+        return this.mes_escrito+" "+this.dia+", "+this.ano;
+    }
+    public String getData3(){
+        return this.dias_no_ano+" "+this.ano;
+    }
 }
